55c64ce91f39ea16339a8fba6c3c83812d9f504e
Mark Tiedemann <www.marktiedemann@gmail.com>
2019-01-22 19:07:47 +0100

    Misc changes

--- test.ps1
+++ test.ps1
@@ -2,6 +2,5 @@ $ErrorActionPreference = 'Stop'
 Write-Output $PSVersionTable
 $Response = Invoke-WebRequest https://github.com/denoland/deno/releases
 $HTMLFile = New-Object -Com HTMLFile
-$HTMLFile.IHTMLDocument3_baseUrl('https://github.com')
-$HTMLFile.IHTMLDocument2_write($Response.Content)
+$HTMLFile.write(@($Response.Content))
 $HTMLFile.getElementsByTagName('a') | ForEach-Object href
