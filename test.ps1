4cfde389d6e05835c72985219b4f96ce63eb454f
Mark Tiedemann <www.marktiedemann@gmail.com>
2019-01-22 18:30:24 +0100

    Try use HTMLFile ComObject

--- test.ps1
+++ test.ps1
@@ -4,3 +4,5 @@ Write-Output $PSVersionTable
 Set-PSDebug -Trace 2
 $Content = (Invoke-WebRequest 'https://github.com/denoland/deno/releases').Content
 Write-Output $Content
+$HTMLFile = New-Object -Com 'HTMLFile'
+$HTMLFile.IHTMLDocument2_write($Content)
