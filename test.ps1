04d478d096cfe9f2766b80933677947b09cc4676
Mark Tiedemann <www.marktiedemann@gmail.com>
2019-01-22 18:46:16 +0100

    Misc changes

--- test.ps1
+++ test.ps1
@@ -4,4 +4,4 @@ Write-Output $PSVersionTable
 $Response = Invoke-WebRequest 'https://github.com/denoland/deno/releases'
 $HTMLFile = New-Object -Com 'HTMLFile'
 $HTMLFile.IHTMLDocument2_write($Response.Content)
-$HTMLFile.anchors
+$HTMLFile | Get-Member
