aa03e6a439d3bdb3af43d9d2a39c050795db42e0
Mark Tiedemann <www.marktiedemann@gmail.com>
2019-01-22 18:40:47 +0100

    Misc changes

--- test.ps1
+++ test.ps1
@@ -1,12 +1,7 @@
 $ErrorActionPreference = 'Stop'
 $VerbosePreference = 'Continue'
 Write-Output $PSVersionTable
-Set-PSDebug -Trace 2
 $Response = Invoke-WebRequest 'https://github.com/denoland/deno/releases'
 $HTMLFile = New-Object -Com 'HTMLFile'
 $HTMLFile.IHTMLDocument2_write($Response.Content)
-$OS = 'win'
-$Zip = 'zip'
-$HTMLFile.all.tags('a') |
-  Where-Object { $_.href -like "/denoland/deno/releases/download/*/deno_${OS}_x64.$Zip" } |
-  ForEach-Object { $_.href }
+$HTMLFile.all.tags('a') | ForEach-Object { $_.href }
