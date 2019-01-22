95e922e2cb9e293aee7c698eda4740d189607921
Mark Tiedemann <www.marktiedemann@gmail.com>
2019-01-22 18:03:53 +0100

    Try .Links property

--- test.ps1
+++ test.ps1
@@ -2,7 +2,7 @@ $ErrorActionPreference = 'Stop'
 $VerbosePreference = 'Continue'
 Set-PSDebug -Trace 2
 $Job = Start-Job -ScriptBlock {
-  Invoke-WebRequest 'https://github.com/denoland/deno/releases'
+  (Invoke-WebRequest 'https://github.com/denoland/deno/releases').Links
 }
 $Job | Wait-Job -Timeout 10
 $Job | Stop-Job
