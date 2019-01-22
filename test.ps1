e4f4ecc509ed0889b2924d565e6a2cef705bdac4
Mark Tiedemann <www.marktiedemann@gmail.com>
2019-01-22 18:12:54 +0100

    Remove job

--- test.ps1
+++ test.ps1
@@ -2,10 +2,4 @@ $ErrorActionPreference = 'Stop'
 $VerbosePreference = 'Continue'
 Write-Output $PSVersionTable
 Set-PSDebug -Trace 2
-$Job = Start-Job -ScriptBlock {
-  Set-PSDebug -Trace 2
-  (Invoke-WebRequest 'https://github.com/denoland/deno/releases').RawContent
-}
-$Job | Wait-Job -Timeout 10
-$Job | Stop-Job
-$Job | Receive-Job
+(Invoke-WebRequest 'https://github.com/denoland/deno/releases').RawContent
