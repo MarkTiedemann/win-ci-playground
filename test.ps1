5e25789c6c7f40031b05acbdcc135742d74e413b
Mark Tiedemann <www.marktiedemann@gmail.com>
2019-01-22 18:13:52 +0100

    Misc changes

--- test.ps1
+++ test.ps1
@@ -2,4 +2,4 @@ $ErrorActionPreference = 'Stop'
 $VerbosePreference = 'Continue'
 Write-Output $PSVersionTable
 Set-PSDebug -Trace 2
-(Invoke-WebRequest 'https://github.com/denoland/deno/releases').RawContent
+(Invoke-WebRequest 'https://github.com/denoland/deno/releases').Content
