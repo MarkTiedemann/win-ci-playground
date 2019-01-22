5fca6e13250e37a95889941745597309c6f2e683
Mark Tiedemann <www.marktiedemann@gmail.com>
2019-01-22 18:42:55 +0100

    Try .write

--- test.ps1
+++ test.ps1
@@ -3,5 +3,5 @@ $VerbosePreference = 'Continue'
 Write-Output $PSVersionTable
 $Response = Invoke-WebRequest 'https://github.com/denoland/deno/releases'
 $HTMLFile = New-Object -Com 'HTMLFile'
-$HTMLFile.IHTMLDocument2_write($Response.Content)
+$HTMLFile.write($Response.Content)
 $HTMLFile.all.tags('a') | ForEach-Object { $_.href }
