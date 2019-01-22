281d799c747a0fff26111ff6cd2272fab5062f9a
Mark Tiedemann <www.marktiedemann@gmail.com>
2019-01-22 19:17:55 +0100

    Misc changes

--- test.ps1
+++ test.ps1
@@ -6,4 +6,6 @@ $HTMLFile.IHTMLDocument2_write($Response.Content)
 $OS = 'win'
 $Zip = 'zip'
 $HTMLFile.getElementsByTagName('a') |
-  ForEach-Object { $_.href -like "about:/denoland/deno/releases/download/*/deno_${OS}_x64.$Zip" }
+  Where-Object { $_.href -like "about:/denoland/deno/releases/download/*/deno_${OS}_x64.$Zip" } |
+  ForEach-Object { $_.href -replace 'about:', 'https://github.com' } |
+  Select-Object -First 2
