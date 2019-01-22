3cf66c3957e92f40485e35f43b4d7cc9e03a6e9b
Mark Tiedemann <www.marktiedemann@gmail.com>
2019-01-22 18:39:10 +0100

    Try getting deno a.href

--- test.ps1
+++ test.ps1
@@ -5,4 +5,8 @@ Set-PSDebug -Trace 2
 $Response = Invoke-WebRequest 'https://github.com/denoland/deno/releases'
 $HTMLFile = New-Object -Com 'HTMLFile'
 $HTMLFile.IHTMLDocument2_write($Response.Content)
-$HTMLFile.all.tags('a')
+$OS = 'win'
+$Zip = 'zip'
+$HTMLFile.all.tags('a') |
+  Where-Object { $_.href -like "/denoland/deno/releases/download/*/deno_${OS}_x64.$Zip" } |
+  ForEach-Object { $_.href }
