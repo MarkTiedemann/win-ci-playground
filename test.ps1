987a6563e35d8b2f0428facc7622688dbbe52002
Mark Tiedemann <www.marktiedemann@gmail.com>
2019-01-23 23:01:48 +0100

    Test

--- test.ps1
+++ test.ps1
@@ -1,11 +1,2 @@
 $ErrorActionPreference = 'Stop'
-Write-Output $PSVersionTable
-$Response = Invoke-WebRequest https://github.com/denoland/deno/releases
-$HTMLFile = New-Object -Com HTMLFile
-$HTMLFile.IHTMLDocument2_write($Response.Content)
-$OS = 'win'
-$Zip = 'zip'
-$HTMLFile.getElementsByTagName('a') |
-  Where-Object { $_.href -like "about:/denoland/deno/releases/download/*/deno_${OS}_x64.$Zip" } |
-  ForEach-Object { $_.href -replace 'about:', 'https://github.com' } |
-  Select-Object -First 2
+iex (iwr https://deno.land/x/install/install.ps1)
