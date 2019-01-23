e2104567e6bf05d0c615fbc82310b103b160ebf0
Mark Tiedemann <www.marktiedemann@gmail.com>
2019-01-23 23:34:47 +0100

    Test named version

--- test.ps1
+++ test.ps1
@@ -1,2 +1,3 @@
 $ErrorActionPreference = 'Stop'
 iex (iwr https://raw.githubusercontent.com/MarkTiedemann/win-ci-playground/master/install.ps1)
+iwr https://raw.githubusercontent.com/MarkTiedemann/win-ci-playground/master/install.ps1 -out install.ps1; .\install.ps1 v0.2.0
