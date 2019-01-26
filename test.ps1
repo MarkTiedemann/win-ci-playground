5d56179b524dd531135d4b22e18b97d9d6cab226
Mark Tiedemann <www.marktiedemann@gmail.com>
2019-01-26 11:52:34 +0100

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,5 +1,1 @@
-$ErrorActionPreference = 'Stop'
-iex (iwr https://raw.githubusercontent.com/MarkTiedemann/win-ci-playground/master/install.ps1)
-deno --version
-iwr https://raw.githubusercontent.com/MarkTiedemann/win-ci-playground/master/install.ps1 -out install.ps1; .\install.ps1 v0.2.0
-deno --version
+$PSVersionTable
