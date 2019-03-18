2b33367ad3db20395d36f5f4de42d0e79cd45865
Mark Tiedemann <www.marktiedemann@gmail.com>
2019-03-19 00:28:02 +0100

    Test

--- test.cmd
+++ test.cmd
@@ -1,7 +1,7 @@
 @echo off
-set current_version="HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion"
-reg query %current_version% /v CurrentVersion
-reg query %current_version% /v CurrentBuild
-reg query %current_version% /v InstallationType
-reg query %current_version% /v EditionID
-reg query %current_version% /v ProductName
+set key="HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion"
+reg query %key% /v CurrentVersion | findstr CurrentVersion
+reg query %key% /v CurrentBuild | findstr CurrentBuild
+reg query %key% /v InstallationType | findstr InstallationType
+reg query %key% /v EditionID | findstr EditionID
+reg query %key% /v ProductName | findstr ProductName
