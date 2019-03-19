5c59e8f6dccfded360ad49de9fc3a678d43f22e5
Mark Tiedemann <www.marktiedemann@gmail.com>
2019-03-19 08:09:48 +0100

    Test

--- ver.ps1
+++ ver.ps1
@@ -3,18 +3,24 @@ $ErrorActionPreference = 'Stop'
 $Reg = "${Env:SystemRoot}\System32\reg.exe"
 $Wmic = "${Env:SystemRoot}\System32\wbem\WMIC.exe"
 
-$Query = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion'
+function Query-Registry ($Key) {
+  $Query = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion'
+  $Result = & $Reg query $Query /v $Key
+  $Match = $Result -match "^\s+$Key\s+REG_SZ\s+(.+)$"
+  $Value = $Match -replace "^\s+$Key\s+REG_SZ\s+(.+)$", '$1'
+  Write-Host "$Key = $Value"
+}
 
-(& $Reg query $Query /v CurrentVersion) -replace '^\s+CurrentVersion\s+REG_SZ\s+(.+)$', '$1'
+Query-Registry CurrentVersion
 & $Wmic os get Version
 
-(& $Reg query $Query /v CurrentBuild) -replace '^\s+CurrentBuild\s+REG_SZ\s+(.+)$', '$1'
+Query-Registry CurrentBuild
 & $Wmic os get BuildNumber
 
-& $Reg query $Query /v InstallationType
+Query-Registry InstallationType
 
-& $Reg query $Query /v EditionID
+Query-Registry EditionID
 
-& $Reg query $Query /v ProductName
+Query-Registry ProductName
 & $Wmic os get Caption
 & $Wmic os get Name
