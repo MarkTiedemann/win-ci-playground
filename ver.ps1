a45887e1a97a156d51833fb9d1c1ea28be06ed9c
Mark Tiedemann <www.marktiedemann@gmail.com>
2019-03-19 08:03:23 +0100

    Test

--- ver.ps1
+++ ver.ps1
@@ -1,14 +1,14 @@
-$ErrorActionPreference = "Stop"
+$ErrorActionPreference = 'Stop'
 
 $Reg = "${Env:SystemRoot}\System32\reg.exe"
 $Wmic = "${Env:SystemRoot}\System32\wbem\WMIC.exe"
 
-$Query = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion"
+$Query = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion'
 
-(& $Reg query $Query /v CurrentVersion) -match "^\s+CurrentVersion\s+REG_SZ\s+(.+)$"
+(& $Reg query $Query /v CurrentVersion) -replace '^\s+CurrentVersion\s+REG_SZ\s+(.+)$', '$1'
 & $Wmic os get Version
 
-& $Reg query $Query /v CurrentBuild
+(& $Reg query $Query /v CurrentBuild) -replace '^\s+CurrentBuild\s+REG_SZ\s+(.+)$', '$1'
 & $Wmic os get BuildNumber
 
 & $Reg query $Query /v InstallationType
