7355c77c2483e75c417fb644c7a921e70d377110
Mark Tiedemann <www.marktiedemann@gmail.com>
2019-03-19 07:57:31 +0100

    Test

--- ver.ps1
+++ ver.ps1
@@ -5,7 +5,7 @@ $Wmic = "${Env:SystemRoot}\System32\wbem\WMIC.exe"
 
 $Query = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion"
 
-& $Reg query $Query /v CurrentVersion
+(& $Reg query $Query /v CurrentVersion) -match "^\s+CurrentVersion\s+REG_SZ\s+(.+)$"
 & $Wmic os get Version
 
 & $Reg query $Query /v CurrentBuild
