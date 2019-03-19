3936ca5a49d7616110553e680c517cac83387778
Mark Tiedemann <www.marktiedemann@gmail.com>
2019-03-19 07:41:49 +0100

    Test

--- ver.cmd
+++ ver.cmd
@@ -1,9 +1,16 @@
 @echo off
 set key="HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion"
+
 reg query %key% /v CurrentVersion | findstr CurrentVersion
+wmic os get Version
+
 reg query %key% /v CurrentBuild | findstr CurrentBuild
+wmic os get BuildNumber
+
 reg query %key% /v InstallationType | findstr InstallationType
+
 reg query %key% /v EditionID | findstr EditionID
+
 reg query %key% /v ProductName | findstr ProductName
-wmic os get caption
-wmic os get /all
+wmic os get Caption
+wmic os get Name
