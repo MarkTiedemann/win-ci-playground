4e9374cfaa2b8a9d0f230384031641a268173403
Mark Tiedemann <www.marktiedemann@gmail.com>
2019-03-27 19:44:47 +0100

    Remove repository registration

--- vs.ps1
+++ vs.ps1
@@ -1,5 +1,4 @@
 $ErrorActionPreference = 'Stop'
-Register-PSRepository -Default
 Install-Module VSSetup -Scope CurrentUser -Force
 foreach ($instance in Get-VSSetupInstance -All -Prerelease) {
   @($instance) | Format-List
