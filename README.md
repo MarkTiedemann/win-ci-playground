6b588fdace9e6e0182240aaf12621b4a54451a8c
Mark Tiedemann <www.marktiedemann@gmail.com>
2020-05-23 01:18:04 +0200

    Try GitHub Actions

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 ## win-ci-playground
 
-A repository I use to test code on Windows via Azure Pipelines.
-
-- [Build](https://dev.azure.com/mark-tiedemann/win-ci-playground/_build)
+A repository I use to test code on Windows.
--- vs.ps1
+++
@@ -1,9 +0,0 @@
-$ErrorActionPreference = 'Stop'
-
-# Install-Module VSSetup -Scope CurrentUser -Force
-# foreach ($instance in Get-VSSetupInstance -All -Prerelease) {
-#   @($instance) | Format-List
-#   $instance.packages | Format-Table
-# }
-
-(Get-CimInstance -Class Win32_OperatingSystem).Caption
